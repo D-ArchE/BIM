ecdae926ec821831b14031207b2d50da23f7cd25
ZGVmaWd1c3Rhdm8K <112300765+ZGVmaWd1c3Rhdm8K@users.noreply.github.com>
2023-04-08 19:22:02 +0100

    bimcalc.py v0.0.3
    added print of total amount of blocks per column
               print of total amount of blocks per wall with column included
               print of total amount of blocks per wall without column included
               print of total amount of columns to create the fortified wall
    //Update
    print of total amount of cement necessary to fully the blank spaces of each block in height
    print of total amount of cement necessary to fully the blank spaces of each block in wight
    print with basis in the total amount of holes in the block and irons per hole is necessary per column
    print with basis in the total amount of holes in the block and irons per hole is necessary per wall without column

--- python/bimcalc.py
+++ python/bimcalc.py
@@ -1,33 +1,45 @@
-def calcular_bim():
-    largura = float(input("Digite a largura do bloco em cm: "))
-    altura = float(input("Digite a altura do bloco em cm: "))
-    comprimento = float(input("Digite o comprimento do bloco em cm: "))
-    altura_coluna = float(input("Digite a altura da coluna em metros: "))
-    distancia_entre_colunas = float(input("Digite a distância entre as colunas em metros: "))
+def calc_bim():
+    largura = float(input("Digite a largura do bloco (em centímetros): "))
+    altura = float(input("Digite a altura do bloco (em centímetros): "))
+    comprimento = float(input("Digite o comprimento do bloco (em centímetros): "))
+    altura_coluna = float(input("Digite a altura da coluna (em metros): "))
+    espaco_largura = float(input("Digite o espaço entre cada bloco na largura (em centímetros): "))
+    espaco_altura = float(input("Digite o espaço entre cada bloco na altura (em centímetros): "))
+    distancia_colunas = float(input("Digite a distância entre uma coluna e outra (em metros): "))
 
-    # Converter medidas para metros
-    largura = largura / 100
-    altura = altura / 100
-    comprimento = comprimento / 100
+    largura_cm = largura + espaco_largura
+    altura_cm = altura + espaco_altura
 
-    # Calcular a quantidade de blocos por coluna
     altura_coluna_cm = altura_coluna * 100
-    qtd_blocos_coluna = altura_coluna_cm // altura
+    distancia_colunas_cm = distancia_colunas * 100
 
-    # Calcular a quantidade de colunas necessárias para uma parede com 2m de distância entre as colunas
-    distancia_entre_colunas_cm = distancia_entre_colunas * 100
-    largura_parede_cm = largura * 100
-    qtd_colunas = largura_parede_cm // (largura + distancia_entre_colunas_cm)
-    if largura_parede_cm % (largura + distancia_entre_colunas_cm) != 0:
-        qtd_colunas += 1
+    quantidade_colunas = int(altura_coluna_cm // altura_cm)
+    quantidade_blocos_coluna = int((altura_coluna_cm - espaco_altura) // altura_cm)
 
-    # Calcular a quantidade total de blocos
-    qtd_blocos_total = qtd_colunas * qtd_blocos_coluna + (largura_parede_cm - distancia_entre_colunas_cm * (qtd_colunas - 1)) * altura_coluna_cm // altura
+    quantidade_blocos_largura = int((43.47 * 100) // largura_cm)
 
-    # Imprimir os resultados
-    print(f"Quantidade de blocos por coluna: {qtd_blocos_coluna:.0f}")
-    print(f"Quantidade de colunas necessárias: {qtd_colunas:.0f}")
-    print(f"Quantidade total de blocos (incluindo os de coluna): {qtd_blocos_total:.0f}")
-    print(f"Quantidade total de blocos (sem os de coluna): {(largura_parede_cm * altura_coluna_cm // altura):.0f}")
+    quantidade_total_blocos = quantidade_blocos_coluna * quantidade_blocos_largura * quantidade_colunas
 
-calcular_bim()
+    quantidade_total_bloco_sem_coluna = (quantidade_blocos_coluna * (quantidade_blocos_largura - 1) + 2) * quantidade_colunas
+
+    espaco_cimento_largura = (quantidade_blocos_largura - 1) * (altura_cm / 100) * (43.47 / 100)
+    espaco_cimento_altura = quantidade_colunas * (distancia_colunas_cm / 100) * (largura_cm / 100)
+
+    quantidade_furos_bloco = int(input("Digite a quantidade de furos/legos do bloco: "))
+    quantidade_ferro_furo = int(input("Digite a quantidade de ferros que vão em cada furo/lego do bloco: "))
+
+    quantidade_total_ferro_coluna = quantidade_blocos_coluna * quantidade_furos_bloco * quantidade_ferro_furo
+    quantidade_total_ferro_parede = (quantidade_colunas - 1) * (quantidade_blocos_largura * quantidade_furos_bloco) * quantidade_ferro_furo
+
+    print("Quantidade total de blocos:", quantidade_total_blocos)
+    print("Quantidade de blocos por coluna:", quantidade_blocos_coluna)
+    print("Quantidade de blocos por espaço de parede:", quantidade_blocos_largura - 1)
+    print("Quantidade total de blocos sem contar as colunas:", quantidade_total_bloco_sem_coluna)
+
+    print("Quantidade de massa de cimento necessária para preencher os espaços entre os blocos na largura: %.2f metros cúbicos" % espaco_cimento_largura)
+    print("Quantidade de massa de cimento necessária para preencher os espaços entre os blocos na altura: %.2f metros cúbicos" % espaco_cimento_altura)
+
+    print("Quantidade total de ferro de construção por coluna:", quantidade_total_ferro_coluna)
+    print("Quantidade total de ferro de construção por espaço de parede:", quantidade_total_ferro_parede)
+
+calc_bim()
