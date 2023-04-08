7c4f186597b2ccb9ec49e30c97ccfb1229cefefa
ZGVmaWd1c3Rhdm8K <112300765+ZGVmaWd1c3Rhdm8K@users.noreply.github.com>
2023-04-08 22:08:34 +0100

    concrete_recipe menu v0.0.2

--- python/concrete_recipe.py
+++ python/concrete_recipe.py
@@ -1,3 +1,6 @@
+import os
+import subprocess
+os.system("clear")
 def sacas_de_cimento_por_metro(proporcao_cimento, peso_saca, densidade_agua=1000):
     # Calcula a quantidade de cimento em quilogramas por metro cúbico
     qtd_cimento_kg = proporcao_cimento * peso_saca
@@ -48,4 +51,56 @@ def concrete():
     side_length = (volume / 2) ** (1/3) # Assuming the concrete is a cube
     print(f"Size of concrete: {side_length:.2f} m x {side_length:.2f} m x {side_length:.2f} m")
 
-concrete()
+def concrete2():
+    # Get input from user
+    meters = float(input("Enter the desired quantity of concrete in meters: "))
+    bags_per_meter = float(input("Enter the number of bags required per meter: "))
+
+    # Calculate material weights
+    bags_needed = meters * bags_per_meter
+    cement_weight = bags_needed * 25
+    sand_weight = cement_weight * 2
+    stone_weight = cement_weight * 4
+    water_liters = cement_weight * 0.5
+
+    # Calculate volume of concrete
+    density = 2400 # kg/m³
+    volume = (cement_weight + sand_weight + stone_weight) / density
+
+    # Calculate total area
+    cement_coverage = 0.025 # assume 25 kg of cement covers 1 sqm
+    total_area = cement_weight / cement_coverage
+
+    # Print results
+    print(f"Total number of bags required: {bags_needed:.2f}")
+    print(f"Total weight of cement: {cement_weight:.2f} kg")
+    print(f"Total weight of sand: {sand_weight:.2f} kg")
+    print(f"Total weight of stone: {stone_weight:.2f} kg")
+    print(f"Total liters of water: {water_liters:.2f} L")
+    print(f"Total volume of concrete: {volume:.2f} m³")
+    print(f"Total area covered: {total_area:.2f} sqm")
+
+    # Print size of concrete
+    side_length = (volume / 2) ** (1/3) # Assuming the concrete is a cube
+    print(f"Size of concrete: {side_length:.2f} m x {side_length:.2f} m x {side_length:.2f} m")
+
+while True:
+    print("\nSelect a function to execute:")
+    print("1. Concrete per Bag/Kg")
+    print("2. Concrete per Meter/Bag")
+    print("0. Exit")
+
+    choice = int(input("Enter your choice: "))
+
+    if choice == 1:
+       os.system("clear")
+       concrete()
+    elif choice == 2:
+       os.system("clear")
+       concrete2()
+    elif choice == 0:
+       os.system("clear")
+       print("Exiting...")
+       break
+    else:
+       print("Invalid choice. Please enter a valid option.")
